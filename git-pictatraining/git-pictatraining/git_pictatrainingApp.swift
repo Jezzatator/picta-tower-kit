32399278912ce083408608ac146424f90894409c
Jérémie P <84284069+Jezzatator@users.noreply.github.com>
2026-02-04 17:39:25 +0100

    Initial commit
    Signed-off-by: Jérémie Patot <jeremie.patot@pictarine.com>

---
+++ git-pictatraining/git-pictatraining/git_pictatrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_pictatrainingApp.swift
+//  git-pictatraining
+//
+//  Created by Jérémie Patot on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct git_pictatrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
